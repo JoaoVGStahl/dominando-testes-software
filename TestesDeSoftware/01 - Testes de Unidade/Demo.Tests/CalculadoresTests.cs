2a09b1032de18b3e0e4bc061db2b65ef054cbf8f
João Girardi <jvitorstahl@hotmail.com>
2022-02-07 13:42:30 -0300

    First Theory

--- TestesDeSoftware/01 - Testes de Unidade/Demo.Tests/CalculadoresTests.cs
+++ TestesDeSoftware/01 - Testes de Unidade/Demo.Tests/CalculadoresTests.cs
@@ -15,7 +15,27 @@ namespace Demo.Tests
             // Act
             var resultado = calculadora.Somar(2, 2);
             //Assert
+            Assert.True(resultado == 4);
             Assert.Equal(4, resultado);
+        }
+        [Theory]
+        [InlineData(1, 1, 2)]
+        [InlineData(2, 2, 4)]
+        [InlineData(4, 2, 6)]
+        [InlineData(7, 3, 10)]
+        [InlineData(6, 6, 12)]
+        [InlineData(9, 9, 18)]
+
+        public void Calculadora_Somar_RetornarSomaValoresCorretos(double v1, double v2, double total)
+        {
+            // Arrange 
+            var calculadora = new Calculadora();
+
+            // Act
+            var resultado = calculadora.Somar(v1, v2);
+
+            // Assert
+            Assert.Equal(total, resultado);
         }
     }
 }
